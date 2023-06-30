15e97e597bec7db0dff573e2ac010d12a6769e53
伍海江 <wuhaijiang@moseeker.com>
2023-06-30 15:44:31 +0800

    init

---
+++ src/test/java/com/whj/jimmerdemo/JimmerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whj.jimmerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimmerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
